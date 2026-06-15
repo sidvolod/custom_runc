7353d68a7def2fe20e90e4d89bc99f75cf80f1ec
sidvolod <sidvolod@gmail.com>
2026-06-15 23:09:05 +0100

    First commit: set up project structure and gitignore

---
+++ include/container.h
@@ -0,0 +1,8 @@
+//
+// Created by Volodymyr on 15/06/2026.
+//
+
+#ifndef CDOCKER_CONTAINER_H
+#define CDOCKER_CONTAINER_H
+
+#endif //CDOCKER_CONTAINER_H
---
+++ src/container.c
@@ -0,0 +1,3 @@
+//
+// Created by Volodymyr on 15/06/2026.
+//
